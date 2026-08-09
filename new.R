cff4e0fd1b1d4e6b5f2e168e603ac93f71909fd4
hannjy <younghj656@gmail.com>
2026-08-10 11:56:00 +1200

    Answered part of q.1 and explored price variable.

--- new.R
+++ new.R
@@ -4,3 +4,23 @@ x <- {1:10}
 x
 
 getwd()
+
+small_march_c <- march_c[c("name", "price","dlc_count","about_the_game","short_description","windows","mac","linux","achievements",
+                           "supported_languages","developers","publishers","categories","genres","estimated_owners",
+                           "average_playtime_forever","peak_ccu","num_reviews_total","pct_pos_total")]
+
+summary(small_march_c)
+
+hist(small_march_c$price)
+
+library(dplyr)
+
+# Ascending order (lowest to highest)
+df_sorted <- small_march_c %>% arrange(desc(price))
+df_sorted$price
+
+filtered_df <- small_march_c %>% filter(price > 199.99)
+filtered_df[,c("name","price")]
+
+hist(filtered_df$price)
+
